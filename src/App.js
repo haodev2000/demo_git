c8ff65dff6786182584acbb38d8fe6ebf7895196
Hao <haodev2000@gmail.com>
2023-11-04 21:28:49 +0700

    video 17

--- src/App.js
+++ src/App.js
@@ -3,28 +3,48 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from './components/MyComponents';
+import React from 'react';
 
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+class App extends React.Component {
 
-  return (
-    <div>
-      Hello Hao
-      <MyComponent />
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
+  state = {
+    name: "Hao",
+    age: 26,
+    address: "Vung Tau"
+  }
+
+  render() {
+    return (
+      <div>
+        Tên : {this.state.name}
+        <br />
+        Tuổi : {this.state.age}
+        <br />
+        Đại chỉ : {this.state.address}
+        <br />
+      </div>
+    );
+  }
+
 }
+
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+//   return (
+
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
